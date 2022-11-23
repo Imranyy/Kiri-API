fd2b5973401b37820ebee6c3c05b9f93cf7d727c
imranyy <imranmatano6@gmail.com>
2022-11-23 13:46:45 +0300

    after deployed

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -58,7 +58,7 @@ const getOneBlog=async(req,res)=>{
 const postNewBlog=async(req,res)=>{
     try {
         const {photo,title,body,author}=req.body;
-        const newBlog=await Blog.create({pic,title,body,author})
+        const newBlog=await Blog.create({photo,title,body,author})
         res.status(200).send(newBlog);
         
     } catch (error) {
